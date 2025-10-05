3e954f84092e0947056fac13f444e9a23bde031c
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-05 12:07:07 +0500

    Setup express server

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log( process.env.BASE_URL )
+import express from "express"
+import session from "express-session"
+import passport from "passport"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+
+const app = express()
+const PORT = process.env.PORT || 3_000
+
+// Session middleware
+app.use( session( {
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		secure: false, // true bo'lsa HTTPS kerak
+		maxAge: 24 * 60 * 60 * 1_000 // 24 soat
+	}
+} ) )
+
+app.use( passport.initialize() )
+app.use( passport.session() )
+
+//
+
+app.listen( PORT, () => {
+
+	console.log( `Server http://localhost:${ PORT } da ishlamoqda` )
+} )
